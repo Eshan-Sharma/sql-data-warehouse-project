cf9a8e17eb62700068184ce24eae19d75d76ecd7
Eshan Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-10-05 09:37:25 +0530

    fix formatting

--- scripts/silver/exploration_erp_cust_az12.sql
+++ scripts/silver/exploration_erp_cust_az12.sql
@@ -10,70 +10,93 @@ USE CATALOG datawarehouse;
 -- ==================================================
 -- Step 1: Check for duplicates in primary key (CID)
 -- ==================================================
-SELECT CID, COUNT(*) AS record_count
+SELECT 
+    CID, 
+    COUNT(*) AS record_count
 FROM bronze.erp_cust_az12
 GROUP BY CID
 HAVING COUNT(*) > 1;
 
--- Check the CID to the cst_key in crm_cust_info
--- Found erp_CUST_AZ12.CID has 'NAS' prefix in some records
-SELECT * from bronze.crm_cust_info;
+-- ==================================================
+-- Step 2: Check CID to the cst_key in crm_cust_info
+-- Note: Found erp_cust_az12.CID has 'NAS' prefix in some records
+-- ==================================================
+SELECT * 
+FROM bronze.crm_cust_info;
 
--- Remove the extra 'NAS' from CID
+-- ==================================================
+-- Step 3: Remove the extra 'NAS' from CID
+-- ==================================================
 SELECT 
-  CASE WHEN CID like 'NAS%' THEN SUBSTRING(CID, 4, LENGTH(CID)) 
-  ELSE CID 
-  END AS CID
+    CASE 
+        WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID, 4, LENGTH(CID)) 
+        ELSE CID 
+    END AS CID
 FROM bronze.erp_cust_az12;
--- Check for distinct values in gender
-SELECT DISTINCT(gen)
-FROM bronze.erp_cust_az12
 
--- Normalize data
+-- ==================================================
+-- Step 4: Check for distinct values in gender
+-- ==================================================
+SELECT DISTINCT GEN
+FROM bronze.erp_cust_az12;
+
+-- ==================================================
+-- Step 5: Normalize gender values
+-- ==================================================
 SELECT 
     CID,
     BDATE,
     CASE 
-        WHEN UPPER(GEN) ='MALE' THEN 'Male'
-        WHEN UPPER(GEN) ='FEMALE' THEN 'Female'
-        WHEN UPPER(GEN) = 'M' THEN 'Male'
-        WHEN UPPER(GEN) = 'F' THEN 'Female'
+        WHEN UPPER(GEN) = 'MALE'   THEN 'Male'
+        WHEN UPPER(GEN) = 'FEMALE' THEN 'Female'
+        WHEN UPPER(GEN) = 'M'      THEN 'Male'
+        WHEN UPPER(GEN) = 'F'      THEN 'Female'
         ELSE 'n/a' 
     END AS GEN
 FROM bronze.erp_cust_az12;
 
--- Check Dates in BDATE
--- Check for range of dates, more then 100 years old is not valid and future dates
+-- ==================================================
+-- Step 6: Check date validity (BDATE)
+-- Rule: more than 100 years old OR future dates are invalid
+-- ==================================================
 SELECT *
 FROM bronze.erp_cust_az12
 WHERE BDATE < DATEADD(YEAR, -100, GETDATE())
    OR BDATE > GETDATE();
 
-   -- Fix 
+-- ==================================================
+-- Step 7: Fix invalid dates
+-- ==================================================
 SELECT 
     CID,
     CASE 
-        WHEN BDATE < DATEADD(YEAR, -100, GETDATE()) OR BDATE > GETDATE() THEN NULL
+        WHEN BDATE < DATEADD(YEAR, -100, GETDATE()) 
+          OR BDATE > GETDATE() 
+        THEN NULL
         ELSE BDATE
     END AS BDATE,
     GEN
 FROM bronze.erp_cust_az12;
 
-
+-- ==================================================
 -- Final Select with all transformations
+-- ==================================================
 SELECT 
-    CASE WHEN CID like 'NAS%' THEN SUBSTRING(CID, 4, LENGTH(CID)) 
+    CASE 
+        WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID, 4, LENGTH(CID)) 
         ELSE CID 
     END AS CID,
     CASE 
-        WHEN BDATE < DATEADD(YEAR, -100, GETDATE()) OR BDATE > GETDATE() THEN NULL
+        WHEN BDATE < DATEADD(YEAR, -100, GETDATE()) 
+          OR BDATE > GETDATE() 
+        THEN NULL
         ELSE BDATE
     END AS BDATE,
     CASE 
-        WHEN UPPER(GEN) ='MALE' THEN 'Male'
-        WHEN UPPER(GEN) ='FEMALE' THEN 'Female'
-        WHEN UPPER(GEN) = 'M' THEN 'Male'
-        WHEN UPPER(GEN) = 'F' THEN 'Female'
+        WHEN UPPER(GEN) = 'MALE'   THEN 'Male'
+        WHEN UPPER(GEN) = 'FEMALE' THEN 'Female'
+        WHEN UPPER(GEN) = 'M'      THEN 'Male'
+        WHEN UPPER(GEN) = 'F'      THEN 'Female'
         ELSE 'n/a' 
     END AS GEN
 FROM bronze.erp_cust_az12;
